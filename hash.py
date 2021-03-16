0733904b4cd72aa1df946f545f5779a93490b3bb
SwissCoding | Python <74186883+swisscoding@users.noreply.github.com>
2021-03-16 21:14:28 +0100

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,14 +1,23 @@
 #!/usr/local/bin/python3
+# Made by @swisscoding on Instagram
 
+from colored import stylize, fg
 import hashlib
-import colored
 
-print(colored.stylize("\n---- | Hash input with sha256 | ----\n", colored.fg("red")))
+# decoration
+print(stylize("\n---- | Hash input with sha256 | ----\n", fg("red")))
+
+# user interaction
 input = input("Import text: ")
-print()
 
-input_encoded = input.encode("utf-8")
-print(f"Encoded input string: {input_encoded}")
+# function
+def hash(s):
+    # encode
+    input_encoded = s.encode("utf-8")
+    print(f"\nEncoded input string: {input_encoded}")
 
-input_hashed = colored.stylize(hashlib.sha256(input_encoded).hexdigest(), colored.fg("red"))
-print(f"Hashed input string: {input_hashed}\n")
+    input_hashed = stylize(hashlib.sha256(input_encoded).hexdigest(), fg("red"))
+    return f"\nHashed input string: {input_hashed}\n"
+
+# output
+print(hash(input))
